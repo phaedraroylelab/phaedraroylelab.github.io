2f7c8b261c914c83c3f5fc9a52d6499a47572018
Kilian-Declercq <kdeclercq3869@gmail.com>
2024-12-16 10:46:22 -0500

    Correct automated version

---
+++ precache-manifest.88eca4e86ee284d9735392fcb47ae6d0.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1eff3150a9de1c90ea533cdd5c374f6",
+    "url": "/labo-langage/index.html"
+  },
+  {
+    "revision": "3e55ff36c2a8d9f473a5",
+    "url": "/labo-langage/static/css/2.dbb74888.chunk.css"
+  },
+  {
+    "revision": "946c5d60775430c3b0f9",
+    "url": "/labo-langage/static/css/4.307928fe.chunk.css"
+  },
+  {
+    "revision": "d581f7ef98270bb7ee4e",
+    "url": "/labo-langage/static/css/6.128a24d2.chunk.css"
+  },
+  {
+    "revision": "afb2e11123f488638aa0",
+    "url": "/labo-langage/static/css/7.16ae888c.chunk.css"
+  },
+  {
+    "revision": "553337df8b6c84b81757",
+    "url": "/labo-langage/static/css/main.14b38750.chunk.css"
+  },
+  {
+    "revision": "53ab0180de72e126cd87",
+    "url": "/labo-langage/static/js/10.1455e5b1.chunk.js"
+  },
+  {
+    "revision": "6896ee03db3541f57b20",
+    "url": "/labo-langage/static/js/11.28b53e40.chunk.js"
+  },
+  {
+    "revision": "3e55ff36c2a8d9f473a5",
+    "url": "/labo-langage/static/js/2.11664572.chunk.js"
+  },
+  {
+    "revision": "7b7e36b0dc1a4ffb7d379428754b0f8b",
+    "url": "/labo-langage/static/js/2.11664572.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05b055e07a36219643af",
+    "url": "/labo-langage/static/js/3.5a681fb8.chunk.js"
+  },
+  {
+    "revision": "b665538fbdb57a21d5f69a6d092e6920",
+    "url": "/labo-langage/static/js/3.5a681fb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "946c5d60775430c3b0f9",
+    "url": "/labo-langage/static/js/4.1f5ea587.chunk.js"
+  },
+  {
+    "revision": "b665538fbdb57a21d5f69a6d092e6920",
+    "url": "/labo-langage/static/js/4.1f5ea587.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fae015634d5473f699d",
+    "url": "/labo-langage/static/js/5.af87718e.chunk.js"
+  },
+  {
+    "revision": "d581f7ef98270bb7ee4e",
+    "url": "/labo-langage/static/js/6.7cb6294b.chunk.js"
+  },
+  {
+    "revision": "afb2e11123f488638aa0",
+    "url": "/labo-langage/static/js/7.ef282c8d.chunk.js"
+  },
+  {
+    "revision": "193500ddf39cb4ce8cae",
+    "url": "/labo-langage/static/js/8.8414638c.chunk.js"
+  },
+  {
+    "revision": "910d5210201f585056e4",
+    "url": "/labo-langage/static/js/9.d1e85a8a.chunk.js"
+  },
+  {
+    "revision": "553337df8b6c84b81757",
+    "url": "/labo-langage/static/js/main.8223657f.chunk.js"
+  },
+  {
+    "revision": "cd742ce6af7d6ccd12b4",
+    "url": "/labo-langage/static/js/runtime-main.62d3dd03.js"
+  }
+]);
